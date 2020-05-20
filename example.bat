776e64915f1fd1cbc0b79103b4cd20d0f506bd2a
jskladal <jskladal@arbes.com>
2020-05-21 01:45:52 +0200

    - pridan navod pro IntelliJ Idea - kdyz se modul pusti jako aplikace, tak je potreba aby v prvnim argumentu byl log

--- example.bat
+++ example.bat
@@ -1,1 +1,20 @@
+Run module as application:
 java -jar target/billing-1.0-SNAPSHOT.jar "420774567453 13-01-2020 18:09:15 13-01-2020 18:12:57"
+
+
+IntelliJ Idea RunConfiguration:
+<component name="ProjectRunConfigurationManager">
+  <configuration default="false" name="MainApp" type="Application" factoryName="Application" nameIsGenerated="true">
+    <option name="MAIN_CLASS_NAME" value="com.phonecompany.MainApp" />
+    <module name="TelephoneBillCalculator" />
+    <extension name="coverage">
+      <pattern>
+        <option name="PATTERN" value="com.phonecompany.*" />
+        <option name="ENABLED" value="true" />
+      </pattern>
+    </extension>
+    <method v="2">
+      <option name="Make" enabled="true" />
+    </method>
+  </configuration>
+</component>
--- src/main/java/com/phonecompany/MainApp.java
+++ src/main/java/com/phonecompany/MainApp.java
@@ -4,16 +4,22 @@ import com.phonecompany.billing.TelephoneBillCalculator;
 
 public class MainApp {
     public static void main(String[] args) {
-        StringBuilder phoneLog = new StringBuilder();
-        phoneLog.append(args[0]);
-        /*phoneLog.append("420774567453 13-01-2020 18:10:15 13-01-2020 18:12:57"+"\n");
-        phoneLog.append("420774567453 13-01-2020 18:10:15 13-01-2020 18:17:57"+"\n");
-        phoneLog.append("420776562353 18-01-2020 08:59:20 18-01-2020 09:10:00"+"\n");
-        phoneLog.append("420776562353 18-01-2020 07:59:20 18-01-2020 08:10:10"+"\n");*/ // 0.5 + 5*1 + 6*0.2 = 6.7
-        // System.out.println(phoneLog.toString());
+        if (args.length == 0) {
+            System.out.println("No log for processing! (For example \"420774567453 13-01-2020 18:10:15 13-01-2020 18:12:57\"");
+        }
+        else {
+            StringBuilder phoneLog = new StringBuilder();
+            phoneLog.append(args[0]);
 
-        TelephoneBillCalculator telephoneBillCalculator = new TelephoneBillCalculator();
-        System.out.println("");
-        System.out.println("Celkova cena vsech novoru: "+telephoneBillCalculator.calculate(phoneLog.toString()));
+           /*phoneLog.append("420774567453 13-01-2020 18:10:15 13-01-2020 18:12:57"+"\n");
+            phoneLog.append("420774567453 13-01-2020 18:10:15 13-01-2020 18:17:57"+"\n");
+            phoneLog.append("420776562353 18-01-2020 08:59:20 18-01-2020 09:10:00"+"\n");
+            phoneLog.append("420776562353 18-01-2020 07:59:20 18-01-2020 08:10:10"+"\n");*/ // 0.5 + 5*1 + 6*0.2 = 6.7
+            // System.out.println(phoneLog.toString());
+
+            TelephoneBillCalculator telephoneBillCalculator = new TelephoneBillCalculator();
+            System.out.println("");
+            System.out.println("Celkova cena vsech novoru: " + telephoneBillCalculator.calculate(phoneLog.toString()));
+        }
     }
 }
